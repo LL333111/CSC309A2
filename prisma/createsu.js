ea9e26560d17551cade99d71196b8a7ba0e298cc
Jiakun <laojiakun@163.com>
2025-10-21 13:24:08 -0400

    finish createsu & seed

--- prisma/createsu.js
+++ prisma/createsu.js
@@ -4,3 +4,34 @@
  *   node prisma/createsu.js clive123 clive.su@mail.utoronto.ca SuperUser123!
  */
 'use strict';
+
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+const args = process.argv;
+
+if (args.length !== 5) {
+  console.error("usage: node prisma/createsu.js utorid email password");
+  process.exit(1);
+}
+
+const utorid = args[2];
+const email = args[3];
+const password = args[4];
+
+async function createsu() {
+  await prisma.user.create({ 
+    data: {
+      utorid,
+      email,
+      password,
+      role: "superuser",
+      name: "",
+      createdAt: new Date().toISOString()
+    }
+  });
+}
+createsu().finally(() => prisma.$disconnect());
+
+
+
--- prisma/seed.js
+++ prisma/seed.js
@@ -3,3 +3,76 @@
  * to do so here.
  */
 'use strict';
+
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+// id        Int      @id @default(autoincrement())
+// utorid    String   @unique
+// name      String
+// email     String   @unique
+// birthday  String   @default("")
+// role      RoleType @default(regular)
+// points    Int      @default(0)
+// createdAt String
+// lastLogin String   @default("")
+// verified  Boolean  @default(false)
+// avatarUrl String   @default("")
+// password  String   @default("")
+
+// utorid: "shepard",
+// name: "shepard chen",
+// email: "shepard.chen@mail.utoronto.ca",
+// birthday: "",
+// role: "",
+// points: ,
+// createdAt: "",
+// lastLogin: "",
+// verified: ,
+// avatarUrl: "",
+async function seedData() {
+  const mockUsers = [
+    {
+      utorid: "shepard",
+      name: "shepard chen",
+      email: "shepard.chen@mail.utoronto.ca",
+      role: "manager",
+      points: 100,
+      createdAt: "2005-09-05T00:00:00.000Z",
+      verified: true,
+    },
+    {
+      utorid: "shaber1",
+      name: "shaber chen",
+      email: "shaber1.chen@mail.utoronto.ca",
+      role: "regular",
+      points: 7,
+      createdAt: "2025-10-01T00:00:00.000Z",
+      verified: false,
+    },
+    {
+      utorid: "shaber2",
+      name: "shaber chen",
+      email: "shaber2.chen@mail.utoronto.ca",
+      role: "regular",
+      points: 12,
+      createdAt: "2025-10-01T00:00:00.000Z",
+      verified: false,
+    },
+    {
+      utorid: "shaber3",
+      name: "shaber chen",
+      email: "shaber3.chen@mail.utoronto.ca",
+      role: "cashier",
+      points: 36,
+      createdAt: "2025-10-01T00:00:00.000Z",
+      verified: false,
+    },
+  ];
+
+  for (const user of mockUsers) {
+    await prisma.user.create({ data: user });
+  }
+}
+
+seedData().finally(() => prisma.$disconnect());
