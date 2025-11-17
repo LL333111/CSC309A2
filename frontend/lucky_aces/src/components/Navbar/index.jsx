93df5f458e2095b125e5630dbea9d6b906e782c5
Jiakun <laojiakun@163.com>
2025-11-17 14:36:15 -0500

    update navbar

--- frontend/lucky_aces/src/components/Navbar/index.jsx
+++ frontend/lucky_aces/src/components/Navbar/index.jsx
@@ -1,16 +1,58 @@
-// import "./style.css"
+import "./style.css"
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [activeSection, setActiveSection] = useState('home');
+  const navigate = useNavigate();
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const handleNavClick = (sectionId, path, e) => {
+    e.preventDefault();
+    setActiveSection(sectionId);
+    setIsMenuOpen(false);
+    navigate(path);
+  };
+
   return (
     <div>
-      <nav>
+      {/* <nav>
         <Link to="/" className="link">Home</Link>
         <Link to="/login" className="link">Login</Link>
         <Link to="/profile" className="link">Profile</Link>
         <Link to="*" className="link">bad</Link>
       </nav>
-      <hr />
+      <hr /> */}
+      <nav className="navbar">
+        <div className="nav-container">
+          <ul className={`nav-menu ${isMenuOpen ? 'active' : ''}`}>
+            <li className="nav-item">
+              <p className={`nav-link ${activeSection === "Home" ? 'active' : ''}`}
+                onClick={(e) => { handleNavClick("Home", "/", e); }}>Home</p>
+            </li>
+            <li className="nav-item">
+              <p className={`nav-link ${activeSection === "Login" ? 'active' : ''}`}
+                onClick={(e) => { handleNavClick("Login", "/login", e); }}>Login</p>
+            </li>
+            <li className="nav-item">
+              <p className={`nav-link ${activeSection === "Profile" ? 'active' : ''}`}
+                onClick={(e) => { handleNavClick("Profile", "/profile", e); }}>Profile</p>
+            </li>
+            <li className="nav-item">
+              <p className={`nav-link ${activeSection === "bad" ? 'active' : ''}`}
+                onClick={(e) => { handleNavClick("bad", "*", e); }}>bad</p>
+            </li>
+          </ul>
+          <button class="hamburger" onClick={toggleMenu}>
+            {isMenuOpen ? '✕' : '☰'}
+          </button>
+        </div>
+      </nav>
     </div>
   )
 }
